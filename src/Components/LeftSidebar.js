c7af7ca1ec5a8d9ab0b5d2b2bd7c4835ec1c38f1
Atta ul Noor <attaulnoor2001@gmail.com>
2023-08-22 13:59:27 +0500

    Modified

--- src/Components/LeftSidebar.js
+++ src/Components/LeftSidebar.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import LeftSidebaroptions from './LeftSidebaroptions'
+import Leftsidebaroptions from './LeftSidebaroptions';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import '../CSS/LeftSidebar.css'
 
 function LeftSidebar() {
   return (
     <div className='LeftSidebar'>
-        <LeftSidebaroptions src='https://media.licdn.com/dms/image/C4E03AQG0HcVoiR1-TA/profile-displayphoto-shrink_800_800/0/1649081167717?e=2147483647&v=beta&t=epvWpYY1NFwvj2VvLAzgeBWzQhAwyLYmssen-VIfJU8' title='Atta ul Noor'/>
-        <LeftSidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/y6/r/i0pziEs5Wj6.png' title='Friends'/>
-        <LeftSidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/y8/r/he-BkogidIc.png' title='Memories'/>
-        <LeftSidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yS/r/k0Svfg6IJtR.png' title='Saved'/>
-        <LeftSidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yZ/r/MhkwI3R0SHP.png' title='Groups'/>
-        <LeftSidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yx/r/XNFKm5WC2yS.png' title='Videos'/>
-        <LeftSidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yC/r/G_tsqBLQpdP.png' title='Marketplace'/>
-        <LeftSidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yb/r/eECk3ceTaHJ.png' title='Feeds'/>
-        <LeftSidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yx/r/WMOYDeEqIYv.png' title='Events'/>
-        <LeftSidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yN/r/ATlxuj_J5ty.png' title='Ads Manager'/>
-        <LeftSidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yq/r/fNPcDZC-2PD.png' title='Crisis response'/>
-        <LeftSidebaroptions Icon={ArrowDropDownIcon} title='See more'/>
+        <Leftsidebaroptions src='https://media.licdn.com/dms/image/C4E03AQG0HcVoiR1-TA/profile-displayphoto-shrink_800_800/0/1649081167717?e=2147483647&v=beta&t=epvWpYY1NFwvj2VvLAzgeBWzQhAwyLYmssen-VIfJU8' title='Atta ul Noor'/>
+        <Leftsidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/y6/r/i0pziEs5Wj6.png' title='Friends'/>
+        <Leftsidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/y8/r/he-BkogidIc.png' title='Memories'/>
+        <Leftsidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yS/r/k0Svfg6IJtR.png' title='Saved'/>
+        <Leftsidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yZ/r/MhkwI3R0SHP.png' title='Groups'/>
+        <Leftsidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yx/r/XNFKm5WC2yS.png' title='Videos'/>
+        <Leftsidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yC/r/G_tsqBLQpdP.png' title='Marketplace'/>
+        <Leftsidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yb/r/eECk3ceTaHJ.png' title='Feeds'/>
+        <Leftsidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yx/r/WMOYDeEqIYv.png' title='Events'/>
+        <Leftsidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yN/r/ATlxuj_J5ty.png' title='Ads Manager'/>
+        <Leftsidebaroptions src='https://static.xx.fbcdn.net/rsrc.php/v3/yq/r/fNPcDZC-2PD.png' title='Crisis response'/>
+        <Leftsidebaroptions Icon={ArrowDropDownIcon} title='See more'/>
     </div>
   )
 }
--- src/Components/LeftSidebaroptions.js
+++ src/Components/LeftSidebaroptions.js
@@ -1,15 +1,15 @@
 import { Avatar, Icon } from '@mui/material'
 import React from 'react'
-import '../CSS/LeftSidebaroptions.css'
+import '../CSS/Leftsidebaroptions.css'
 
-function LeftSidebaroptions({src, Icon, title}) {
+function Leftsidebaroptions({src, Icon, title}) {
   return (
-    <div className='leftSidebarRow'>
-        <p id='LeftSidebarSvg'>{src && <Avatar src={src} />}</p>
-        <p id='LeftSidebarIcon'>{Icon && <Icon /> }</p>
-        <p id='LeftSidebarTitle'>{title}</p>
+    <div className='leftsidebarRow'>
+        {src && <Avatar src={src} />}
+        {Icon && <Icon /> }
+        <p>{title}</p>
     </div>
   )
 }
 
-export default LeftSidebaroptions
+export default Leftsidebaroptions
