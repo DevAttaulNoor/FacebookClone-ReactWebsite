b86e357957b797875d0040f77b49ce91fac6054b
Atta ul Noor <attaulnoor2001@gmail.com>
2023-11-03 15:55:45 +0500

    HomePage Messages Modified

--- src/Components/HomePage/HomePage_Rightbar_FriendsList.jsx
+++ src/Components/HomePage/HomePage_Rightbar_FriendsList.jsx
@@ -1,6 +1,7 @@
 import '../../CSS/HomePage/HomePage_Rightbar_FriendsList.css'
 import React, { useEffect, useState } from 'react'
 import { Avatar } from '@mui/material';
+import { db } from '../BackendRelated/Firebase';
 import { useStateValue } from '../BackendRelated/StateProvider'
 import { fetchFriendsData, fetchFriendDetailsData } from '../FriendsPage/FriendsPage_AllFriends_Leftbar';
 import EmojiPicker from 'emoji-picker-react';
@@ -11,20 +12,15 @@ import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
-import { db } from '../BackendRelated/Firebase';
 
-function HomePage_Rightbar_FriendsList({ close }) {
+function HomePage_Rightbar_FriendsList() {
     const [{ user }] = useStateValue();
     const [friends, setFriends] = useState([]);
-
+    const [messages, setMessages] = useState([]);
+    const [messageInput, setMessageInput] = useState('');
     const [selectedFriend, setSelectedFriend] = useState(null);
     const [isDialogVisible, setIsDialogVisible] = useState(false);
     const [isEmojiPickerVisible, setIsEmojiPickerVisible] = useState(false);
-
-    const [messages, setMessages] = useState([]); // Store messages
-    const [messageInput, setMessageInput] = useState('');
-
-
 
     useEffect(() => {
         // Fetch friends data when user.uid changes
@@ -38,51 +34,118 @@ function HomePage_Rightbar_FriendsList({ close }) {
         }
     }, [friends]);
 
-    const handleFriendClick = (friend) => {
-        // Set the selected friend when a friend is clicked
-        setSelectedFriend(friend);
-        setIsDialogVisible(true); // Show the friend's message box
-
-        // Fetch messages for the selected friend and set them in the state
-        db.collection('Messages')
-            .where('recipient', '==', user.uid)
-            .where('sender', '==', friend.id)
-            .onSnapshot((snapshot) => {
-                const messages = [];
-                snapshot.forEach((doc) => {
-                    messages.push(doc.data());
-                });
-                setMessages(messages);
-            });
-    };
-
     const handleEmojiIconClick = () => {
         setIsEmojiPickerVisible(!isEmojiPickerVisible); // Toggle EmojiPicker visibility
     };
 
-    // Handle sending a message
+    const handleFriendClick = (friend) => {
+        // Set the selected friend when a friend is clicked
+        setSelectedFriend(friend);
+        setIsDialogVisible(true);
+
+        // Fetch messages for the selected friend and set them in the state
+        db.collection('Messages')
+            .where('person1', '==', user.uid)
+            .where('person2', '==', friend.friendUid)
+            .get()
+            .then((querySnapshot1) => {
+                // Query for messages where user.uid is person1 and friend.friendUid is person2
+
+                const messages1 = [];
+                querySnapshot1.forEach((doc) => {
+                    messages1.push(doc.data());
+                });
+
+                db.collection('Messages')
+                    .where('person1', '==', friend.friendUid)
+                    .where('person2', '==', user.uid)
+                    .get()
+                    .then((querySnapshot2) => {
+                        // Query for messages where friend.friendUid is person1 and user.uid is person2
+
+                        const messages2 = [];
+                        querySnapshot2.forEach((doc) => {
+                            messages2.push(doc.data());
+                        });
+
+                        // Combine the messages from both queries
+                        const combinedMessages = messages1.concat(messages2);
+                        setMessages(combinedMessages);
+                    })
+                    .catch((error) => {
+                        console.error('Error getting messages for person1 === friendUid: ', error);
+                    });
+            })
+            .catch((error) => {
+                console.error('Error getting messages for person1 === user.uid: ', error);
+            });
+
+    };
+
     const sendMessage = () => {
         if (messageInput.trim() === '') {
+            return;
+        }
+
+        if (!selectedFriend || !selectedFriend.friendUid || !user.uid) {
+            // Handle the case where recipient, sender, or selectedFriend is undefined
+            console.error('Recipient or sender is undefined');
             return;
         }
 
         // Update messages state
         const newMessage = {
             text: messageInput,
-            sender: user.uid,
-            recipient: selectedFriend.id,
+            person1: user.uid,
+            person2: selectedFriend.friendUid,
             timestamp: new Date().toISOString(),
         };
 
         setMessages([...messages, newMessage]);
 
         // Save the message in Firestore
+        // db.collection('Messages').collection('conversations').add(newMessage);
+
         db.collection('Messages').add(newMessage);
 
         // Clear the message input
         setMessageInput('');
     };
 
+    const timeAgo = (timestamp) => {
+        if (!timestamp || !timestamp.toDate) {
+            return "0 second ago"
+        }
+        const currentDate = new Date();
+        const postDate = timestamp.toDate();
+        const seconds = Math.floor((currentDate - postDate) / 1000);
+        const secondsDifference = Math.max(seconds, 1);
+        const periods = {
+            decade: 315360000,
+            year: 31536000,
+            month: 2628000,
+            week: 604800,
+            day: 86400,
+            hour: 3600,
+            minute: 60,
+            second: 1,
+        };
+
+        let elapsed = 0;
+        let granularity = 0;
+        let unit = '';
+
+        for (const period in periods) {
+            elapsed = Math.floor(secondsDifference / periods[period]);
+
+            if (elapsed >= 1) {
+                granularity = elapsed;
+                unit = period;
+                break;
+            }
+        }
+        return `${granularity} ${unit}${granularity > 1 ? 's' : ''} ago`;
+    };
 
     return (
         <div className='homepage_rightbar_friendList'>
@@ -103,6 +166,7 @@ function HomePage_Rightbar_FriendsList({ close }) {
                         key={friend.id}
                         onClick={() => handleFriendClick(friend)} // Handle friend click
                     >
+                        {/* {console.log(friend.friendUid)} */}
                         <Avatar src={friend.photoURL} />
                         <p>{friend.username}</p>
                     </div>
@@ -126,14 +190,18 @@ function HomePage_Rightbar_FriendsList({ close }) {
                         </div>
 
                         <div className="FriendMessages_Middle">
-                            <Avatar src={selectedFriend.photoURL} />
-                            <h3>{selectedFriend.username}</h3>
-                            <p>Facebook</p>
-                            <p>You're friends on Facebook</p>
+                            <div className='friendIntro'>
+                                <Avatar src={selectedFriend.photoURL} />
+                                <h3>{selectedFriend.username}</h3>
+                                <p>Facebook</p>
+                                <p>You're friends on Facebook</p>
+                            </div>
+
 
                             {messages.map((message) => (
-                                <div key={message.timestamp} className={`message ${message.sender === user.uid ? 'sent' : 'received'}`}>
-                                    {message.text}
+                                <div key={timeAgo(message.timestamp)} className={`message ${message.person1 === user.uid ? 'sent' : 'received'}`}>
+                                    <p id='messageContent'>{message.text}</p>
+                                    <p id='messageTimestamp'>{timeAgo(message.timestamp)}</p>
                                 </div>
                             ))}
                         </div>
