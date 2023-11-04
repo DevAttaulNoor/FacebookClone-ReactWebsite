fb765dbae085c6dd7d2a24a6707c89d89b779f4b
Atta ul Noor <attaulnoor2001@gmail.com>
2023-11-04 14:39:46 +0500

    HomePage Messages Modified

--- src/Components/HomePage/HomePage_Messages.jsx
+++ src/Components/HomePage/HomePage_Messages.jsx
@@ -8,10 +8,10 @@ import CloseIcon from '@mui/icons-material/Close';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
+import { useStateValue } from '../BackendRelated/StateProvider';
 
-function HomePage_Messages({ close }) {
+function HomePage_Messages({ close, closeBox, handleSelectedFriend }) {
     const [comment, setComment] = useState('');
-    const [message, setMessage] = useState("");
     const [searchText, setSearchText] = useState('');
     const [selectedUsers, setSelectedUsers] = useState([]);
     const [matchingUsernames, setMatchingUsernames] = useState([]);
@@ -19,6 +19,12 @@ function HomePage_Messages({ close }) {
     const [showEmojiPicker, setShowEmojiPicker] = useState(false);
     const [isDialogVisible, setIsDialogVisible] = useState(false);
     const dialogBoxRef = useRef(null);
+
+
+    const [{ user }] = useStateValue();
+    const [messages, setMessages] = useState([]);
+    const [messageInput, setMessageInput] = useState('');
+    const [isEmojiPickerVisible, setIsEmojiPickerVisible] = useState(false);
 
     const toggleUserSelection = (userId, username, photoURL) => {
         const isUserSelected = selectedUsers.some(user => user.userId === userId);
@@ -45,25 +51,6 @@ function HomePage_Messages({ close }) {
     const toggleDialog = () => {
         setIsDialogVisible(!isDialogVisible);
     };
-
-    useEffect(() => {
-        const handleOutsideClick = (e) => {
-            if (
-                isSearchBoxVisible &&
-                !document.querySelector(".header_search").contains(e.target)
-            ) {
-                setIsSearchBoxVisible(false);
-                setSearchText('');
-            }
-        };
-
-        window.addEventListener("click", handleOutsideClick);
-
-        // Cleanup the event listener when the component unmounts
-        return () => {
-            window.removeEventListener("click", handleOutsideClick);
-        };
-    }, [isSearchBoxVisible]);
 
     useEffect(() => {
         if (searchText === '') {
@@ -95,83 +82,251 @@ function HomePage_Messages({ close }) {
             });
     }, [searchText]);
 
+
+
+
+
+
+    const handleEmojiIconClick = () => {
+        setIsEmojiPickerVisible(!isEmojiPickerVisible); // Toggle EmojiPicker visibility
+    };
+
+    const sendMessage = async () => {
+        if (messageInput.trim() === '') {
+            return;
+        }
+
+        if (!handleSelectedFriend || !handleSelectedFriend.friendUid || !user.uid) {
+            console.error('Recipient or sender is undefined');
+            return;
+        }
+
+        const chatId = user.uid < handleSelectedFriend.friendUid
+            ? `${user.uid}_${handleSelectedFriend.friendUid}`
+            : `${handleSelectedFriend.friendUid}_${user.uid}`;
+
+        const newMessage = {
+            text: messageInput,
+            sender: user.uid,
+            recipient: handleSelectedFriend.friendUid,
+            timestamp: new Date(),
+        };
+
+        // Retrieve the existing messages for the chat
+        const chatDocRef = db.collection('Chats').doc(chatId);
+        const chatDoc = await chatDocRef.get();
+
+        if (chatDoc.exists) {
+            const existingMessages = chatDoc.data().messages || [];
+            const updatedMessages = [...existingMessages, newMessage];
+
+            // Update the messages field in the document
+            await chatDocRef.update({ messages: updatedMessages });
+        }
+
+        else {
+            // If the chat document doesn't exist, create it with the new message
+            await chatDocRef.set({ messages: [newMessage] });
+        }
+
+        // Clear the message input
+        setMessageInput('');
+    };
+
+    const timeAgowithInitials = (timestamp) => {
+        if (!timestamp || !timestamp.toDate) {
+            return "0s"
+        }
+        const currentDate = new Date();
+        const postDate = timestamp.toDate();
+        const seconds = Math.floor((currentDate - postDate) / 1000);
+        const secondsDifference = Math.max(seconds, 1);
+        const periods = {
+            D: 315360000,
+            Y: 31536000,
+            M: 2628000,
+            w: 604800,
+            d: 86400,
+            h: 3600,
+            m: 60,
+            s: 1,
+        };
+
+        let elapsed = 0;
+        let granularity = 0;
+        let unit = '';
+
+        for (const period in periods) {
+            elapsed = Math.floor(secondsDifference / periods[period]);
+
+            if (elapsed >= 1) {
+                granularity = elapsed;
+                unit = period;
+                break;
+            }
+        }
+        return `${granularity}${unit}${granularity > 1 ? '' : ''}`;
+    };
+
+    useEffect(() => {
+        if (handleSelectedFriend) {
+            const userUid = user.uid;
+            const friendUid = handleSelectedFriend.friendUid;
+
+            const chatId = userUid < friendUid
+                ? `${userUid}_${friendUid}`
+                : `${friendUid}_${userUid}`;
+
+            const chatCollection = db.collection('Chats').doc(chatId);
+
+            const unsubscribe = chatCollection.onSnapshot((doc) => {
+                if (doc.exists) {
+                    const chatData = doc.data();
+                    if (chatData.messages) {
+                        setMessages(chatData.messages);
+                    } else {
+                        setMessages([]); // No messages available
+                    }
+                } else {
+                    setMessages([]); // Chat document doesn't exist
+                }
+            });
+
+            return () => {
+                // Unsubscribe from the snapshot listener when the component unmounts.
+                unsubscribe();
+            };
+        }
+    }, [handleSelectedFriend, user.uid]);
+
     return (
-        <div className='HomePageMessages'>
-            <div className="HomePageMessages_Top">
-                <div className='HomePageMessages_TopLeft'>
-                    <p>New message</p>
-                </div>
-
-                <div className='HomePageMessages_TopRight'>
-                    <HorizontalRuleIcon />
-                    <CloseIcon onClick={close} />
-                </div>
-            </div>
-
-            <div className="HomePageMessages_Middle">
-                <p id='toText'>To: </p>
-
-                <div className="HomePageMessages_MiddleRight">
-                    {selectedUsers.length > 0 && (
-                        <div className="selectedUserSection">
-                            {selectedUsers.map((user) => (
-                                <div key={user.userId} className="selectedUser">
-                                    <p>{user.username}</p>
-                                    <CloseIcon onClick={() => deselectUser(user.userId)} />
-                                </div>
-                            ))}
+        <>
+            {isDialogVisible && (
+                <div className='HomePageMessages'>
+                    <div className="HomePageMessages_Top">
+                        <div className='HomePageMessages_TopLeft'>
+                            <p>New message</p>
                         </div>
-                    )}
-                    <input
-                        type="text"
-                        value={searchText}
-                        onChange={(e) => setSearchText(e.target.value)}
-                        onClick={() => setIsSearchBoxVisible(!isSearchBoxVisible)}
-                    />
-                </div>
-            </div>
 
-            <div className="HomePageMessages_Bottom">
-                {selectedUsers.length > 0 && (
-                    <div className="HomePageMessages_BottomInner">
-                        {selectedUsers.map((user) => (
-                            <div key={user.userId} className="HomePageMessages_BottomInner_Top">
-                                <Avatar src={user.photoURL} />
-                                <p>{user.username}</p>
-                            </div>
-                        ))}
-
-                        <div className='HomePageMessages_BottomInner_Bottom'>
-                            <AddCircleIcon />
-                            <div className='inputSection'>
-                                <input type='text' placeholder='Aa' value={comment} onChange={(e) => setComment(e.target.value)} />
-                                <EmojiEmotionsIcon className='emojiIcon' onClick={toggleDialog} ref={dialogBoxRef} />
-
-                            </div>
-                            {isDialogVisible && (
-                                <EmojiPicker onEmojiClick={handleEmojiClick} />
-                            )}
-                            <SendIcon />
+                        <div className='HomePageMessages_TopRight'>
+                            <HorizontalRuleIcon />
+                            <CloseIcon onClick={close} />
                         </div>
                     </div>
-                )}
 
-                {searchText !== '' && matchingUsernames.length === 0 ? (
-                    <p id='noMatch'>No matches</p>
-                ) : (
-                    matchingUsernames.map((user) => (
-                        <div
-                            key={user.id}
-                            className={`HomePageMessages_BottomResults ${selectedUsers.includes(user.id) ? "selected" : ""}`}
-                            onClick={() => toggleUserSelection(user.id, user.username, user.photoURL)}
-                        >
-                            <Avatar src={user.photoURL} />
-                            <p>{user.username}</p>
+                    <div className="HomePageMessages_Middle">
+                        <p id='toText'>To: </p>
+
+                        <div className="HomePageMessages_MiddleRight">
+                            {selectedUsers.length > 0 && (
+                                <div className="selectedUserSection">
+                                    {selectedUsers.map((user) => (
+                                        <div key={user.userId} className="selectedUser">
+                                            <p>{user.username}</p>
+                                            <CloseIcon onClick={() => deselectUser(user.userId)} />
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                            <input
+                                type="text"
+                                value={searchText}
+                                onChange={(e) => setSearchText(e.target.value)}
+                                onClick={() => setIsSearchBoxVisible(!isSearchBoxVisible)}
+                            />
                         </div>
-                    ))
-                )}
-            </div>
-        </div>
+                    </div>
+
+                    <div className="HomePageMessages_Bottom">
+                        {selectedUsers.length > 0 && (
+                            <div className="HomePageMessages_BottomInner">
+                                {selectedUsers.map((user) => (
+                                    <div key={user.userId} className="HomePageMessages_BottomInner_Top">
+                                        <Avatar src={user.photoURL} />
+                                        <p>{user.username}</p>
+                                    </div>
+                                ))}
+
+                                <div className='HomePageMessages_BottomInner_Bottom'>
+                                    <AddCircleIcon />
+                                    <div className='inputSection'>
+                                        <input type='text' placeholder='Aa' value={comment} onChange={(e) => setComment(e.target.value)} />
+                                        <EmojiEmotionsIcon className='emojiIcon' onClick={toggleDialog} ref={dialogBoxRef} />
+
+                                    </div>
+                                    {isDialogVisible && (
+                                        <EmojiPicker onEmojiClick={handleEmojiClick} />
+                                    )}
+                                    <SendIcon />
+                                </div>
+                            </div>
+                        )}
+
+                        {searchText !== '' && matchingUsernames.length === 0 ? (
+                            <p id='noMatch'>No matches</p>
+                        ) : (
+                            matchingUsernames.map((user) => (
+                                <div
+                                    key={user.id}
+                                    className={`HomePageMessages_BottomResults ${selectedUsers.includes(user.id) ? "selected" : ""}`}
+                                    onClick={() => toggleUserSelection(user.id, user.username, user.photoURL)}
+                                >
+                                    <Avatar src={user.photoURL} />
+                                    <p>{user.username}</p>
+                                </div>
+                            ))
+                        )}
+                    </div>
+                </div>
+            )}
+
+            {handleSelectedFriend && (
+                <div className='FriendMessages'>
+                    <div className="FriendMessages_Top">
+                        <div className='FriendMessages_TopLeft'>
+                            <Avatar src={handleSelectedFriend.photoURL} />
+                            <p>{handleSelectedFriend.username}</p>
+                        </div>
+
+                        <div className='FriendMessages_TopRight'>
+                            <HorizontalRuleIcon />
+                            <CloseIcon onClick={closeBox} />
+                        </div>
+                    </div>
+
+                    <div className="FriendMessages_Middle">
+                        <div className='friendIntro'>
+                            <Avatar src={handleSelectedFriend.photoURL} />
+                            <h3>{handleSelectedFriend.username}</h3>
+                            <p>Facebook</p>
+                            <p>You're friends on Facebook</p>
+                        </div>
+
+                        {messages.map((message) => (
+                            <div key={message.timestamp} className={`message ${message.sender === user.uid ? 'sent' : 'received'}`}>
+                                <p id='messageContent'>{message.text}</p>
+                                <p id='messageTimestamp'>{timeAgowithInitials(message.timestamp)}</p>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className='FriendMessages_Bottom'>
+                        <AddCircleIcon />
+                        <div className='inputSection'>
+                            <input
+                                type='text'
+                                placeholder='Aa'
+                                value={messageInput}
+                                onChange={(e) => setMessageInput(e.target.value)}
+                            />
+                            <EmojiEmotionsIcon className='emojiIcon' onClick={handleEmojiIconClick} />
+                        </div>
+                        {isEmojiPickerVisible && <EmojiPicker />}
+                        <SendIcon onClick={sendMessage} />
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
 
--- src/Components/HomePage/HomePage_Rightbar_FriendsList.jsx
+++ src/Components/HomePage/HomePage_Rightbar_FriendsList.jsx
@@ -1,26 +1,27 @@
 import '../../CSS/HomePage/HomePage_Rightbar_FriendsList.css'
 import React, { useEffect, useState } from 'react'
 import { Avatar } from '@mui/material';
-import { db } from '../BackendRelated/Firebase';
 import { useStateValue } from '../BackendRelated/StateProvider'
 import { fetchFriendsData, fetchFriendDetailsData } from '../FriendsPage/FriendsPage_AllFriends_Leftbar';
-import EmojiPicker from 'emoji-picker-react';
-import SendIcon from '@mui/icons-material/Send';
-import CloseIcon from '@mui/icons-material/Close';
+import HomePage_Messages from '../HomePage/HomePage_Messages';
 import SearchIcon from '@mui/icons-material/Search';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
-import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 
 function HomePage_Rightbar_FriendsList() {
     const [{ user }] = useStateValue();
     const [friends, setFriends] = useState([]);
-    const [messages, setMessages] = useState([]);
-    const [messageInput, setMessageInput] = useState('');
+    const [messageBox, setMessageBox] = useState(false);
     const [selectedFriend, setSelectedFriend] = useState(null);
-    const [isDialogVisible, setIsDialogVisible] = useState(false);
-    const [isEmojiPickerVisible, setIsEmojiPickerVisible] = useState(false);
+
+    const openMessageBox = (friend) => {
+        setMessageBox(true);
+        setSelectedFriend(friend);
+    };
+
+    const closeMessageBox = () => {
+        setMessageBox(false);
+        setSelectedFriend(null);
+    }
 
     useEffect(() => {
         // Fetch friends data when user.uid changes
@@ -34,198 +35,33 @@ function HomePage_Rightbar_FriendsList() {
         }
     }, [friends]);
 
-    const handleEmojiIconClick = () => {
-        setIsEmojiPickerVisible(!isEmojiPickerVisible); // Toggle EmojiPicker visibility
-    };
-    
-    const handleFriendClick = (friend) => {
-        setSelectedFriend(friend);
-        setIsDialogVisible(true);
-    };
-
-    const sendMessage = async () => {
-        if (messageInput.trim() === '') {
-            return;
-        }
-
-        if (!selectedFriend || !selectedFriend.friendUid || !user.uid) {
-            console.error('Recipient or sender is undefined');
-            return;
-        }
-
-        const chatId = user.uid < selectedFriend.friendUid
-            ? `${user.uid}_${selectedFriend.friendUid}`
-            : `${selectedFriend.friendUid}_${user.uid}`;
-
-        const newMessage = {
-            text: messageInput,
-            sender: user.uid,
-            recipient: selectedFriend.friendUid,
-            timestamp: new Date(),
-        };
-
-        // Retrieve the existing messages for the chat
-        const chatDocRef = db.collection('Chats').doc(chatId);
-        const chatDoc = await chatDocRef.get();
-
-        if (chatDoc.exists) {
-            const existingMessages = chatDoc.data().messages || [];
-            const updatedMessages = [...existingMessages, newMessage];
-
-            // Update the messages field in the document
-            await chatDocRef.update({ messages: updatedMessages });
-        }
-
-        else {
-            // If the chat document doesn't exist, create it with the new message
-            await chatDocRef.set({ messages: [newMessage] });
-        }
-
-        // Clear the message input
-        setMessageInput('');
-    };
-
-    const timeAgowithInitials = (timestamp) => {
-        if (!timestamp || !timestamp.toDate) {
-            return "0s"
-        }
-        const currentDate = new Date();
-        const postDate = timestamp.toDate();
-        const seconds = Math.floor((currentDate - postDate) / 1000);
-        const secondsDifference = Math.max(seconds, 1);
-        const periods = {
-            D: 315360000,
-            Y: 31536000,
-            M: 2628000,
-            w: 604800,
-            d: 86400,
-            h: 3600,
-            m: 60,
-            s: 1,
-        };
-
-        let elapsed = 0;
-        let granularity = 0;
-        let unit = '';
-
-        for (const period in periods) {
-            elapsed = Math.floor(secondsDifference / periods[period]);
-
-            if (elapsed >= 1) {
-                granularity = elapsed;
-                unit = period;
-                break;
-            }
-        }
-        return `${granularity}${unit}${granularity > 1 ? '' : ''}`;
-    };
-
-    useEffect(() => {
-        if (selectedFriend) {
-            const userUid = user.uid;
-            const friendUid = selectedFriend.friendUid;
-    
-            const chatId = userUid < friendUid
-                ? `${userUid}_${friendUid}`
-                : `${friendUid}_${userUid}`;
-    
-            const chatCollection = db.collection('Chats').doc(chatId);
-    
-            const unsubscribe = chatCollection.onSnapshot((doc) => {
-                if (doc.exists) {
-                    const chatData = doc.data();
-                    if (chatData.messages) {
-                        setMessages(chatData.messages);
-                    } else {
-                        setMessages([]); // No messages available
-                    }
-                } else {
-                    setMessages([]); // Chat document doesn't exist
-                }
-            });
-    
-            return () => {
-                // Unsubscribe from the snapshot listener when the component unmounts.
-                unsubscribe();
-            };
-        }
-    }, [selectedFriend, user.uid]);
-    
     return (
-        <div className='homepage_rightbar_friendList'>
-            <div className="homepage_rightbar_friendListTop">
-                <div className="homepage_rightbar_friendListTopLeft">
+        <div className='homepageRightbarFriendList'>
+            <div className="homepageRightbarFriendList_Top">
+                <div className="homepageRightbarFriendList_TopLeft">
                     <h4>Contacts</h4>
                 </div>
-                <div className="homepage_rightbar_friendListTopRight">
+
+                <div className="homepageRightbarFriendList_TopRight">
                     <SearchIcon />
                     <MoreHorizIcon />
                 </div>
             </div>
 
-            <div className="homepage_rightbar_friendListBody">
+            <div className="homepageRightbarFriendList_Bottom">
                 {friends.map((friend) => (
                     <div
-                        className="homepage_rightbar_friendListBody_Options"
+                        className="homepageRightbarFriendList_BottomOption"
                         key={friend.id}
-                        onClick={() => handleFriendClick(friend)} // Handle friend click
+                        onClick={() => openMessageBox(friend)}
                     >
-                        {/* {console.log(friend.friendUid)} */}
                         <Avatar src={friend.photoURL} />
                         <p>{friend.username}</p>
                     </div>
                 ))}
-
-                {selectedFriend && (
-                    <div className='FriendMessages'>
-                        <div className="FriendMessages_Top">
-                            <div className='FriendMessages_TopLeft'>
-                                <Avatar src={selectedFriend.photoURL} />
-                                <p>{selectedFriend.username}</p>
-                            </div>
-
-                            <div className='FriendMessages_TopRight'>
-                                <HorizontalRuleIcon />
-                                <CloseIcon onClick={() => {
-                                    setSelectedFriend(null);
-                                    setIsEmojiPickerVisible(false); // Close the EmojiPicker
-                                }} />
-                            </div>
-                        </div>
-
-                        <div className="FriendMessages_Middle">
-                            <div className='friendIntro'>
-                                <Avatar src={selectedFriend.photoURL} />
-                                <h3>{selectedFriend.username}</h3>
-                                <p>Facebook</p>
-                                <p>You're friends on Facebook</p>
-                            </div>
-
-                            {messages.map((message) => (
-                                <div key={message.timestamp} className={`message ${message.sender === user.uid ? 'sent' : 'received'}`}>
-                                    <p id='messageContent'>{message.text}</p>
-                                    <p id='messageTimestamp'>{timeAgowithInitials(message.timestamp)}</p>
-                                </div>
-                            ))}
-                        </div>
-
-                        <div className='FriendMessages_Bottom'>
-                            <AddCircleIcon />
-                            <div className='inputSection'>
-                                <input
-                                    type='text'
-                                    placeholder='Aa'
-                                    value={messageInput}
-                                    onChange={(e) => setMessageInput(e.target.value)}
-                                />
-                                <EmojiEmotionsIcon className='emojiIcon' onClick={handleEmojiIconClick} />
-                            </div>
-                            {isEmojiPickerVisible && <EmojiPicker />}
-                            <SendIcon onClick={sendMessage} />
-                        </div>
-                    </div>
-                )}
             </div>
+
+            <HomePage_Messages handleSelectedFriend={selectedFriend} closeBox={closeMessageBox} />
         </div>
     )
 }
