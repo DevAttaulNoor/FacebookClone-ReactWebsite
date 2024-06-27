3844c10faa5857d81505a3a1466b8606b7eddb85
Atta ul Noor <attaulnoor2001@gmail.com>
2024-06-27 15:25:22 +0500

    FriendPage Responsive

--- src/Components/FriendPage/FriendPage.jsx
+++ src/Components/FriendPage/FriendPage.jsx
@@ -20,24 +20,24 @@ function FriendPage() {
 
                 <div className="friendpageLeftbarBottom">
                     <NavLink to="/friendpage/" activeclassname="active">
-                        <div className="friendpageLeftbarBottomOptions">
-                            <div className="friendpageLeftbarBottomOptionsLeft">
+                        <div className="friendpageLeftbarBottomOption">
+                            <div className="friendpageLeftbarBottomOptionLeft">
                                 <SupervisorAccountIcon />
                                 <p>Home</p>
                             </div>
-                            <div className="friendpageLeftbarBottomOptionsRight">
+                            <div className="friendpageLeftbarBottomOptionRight">
                                 <ArrowForwardIosIcon />
                             </div>
                         </div>
                     </NavLink>
 
                     <NavLink to="/friendpage/friendReqs" activeclassname="active">
-                        <div className="friendpageLeftbarBottomOptions">
-                            <div className="friendpageLeftbarBottomOptionsLeft">
+                        <div className="friendpageLeftbarBottomOption">
+                            <div className="friendpageLeftbarBottomOptionLeft">
                                 <PersonAddAlt1Icon />
                                 <p>Friend Requests</p>
                             </div>
-                            <div className="friendpageLeftbarBottomOptionsRight">
+                            <div className="friendpageLeftbarBottomOptionRight">
                                 <ArrowForwardIosIcon />
                             </div>
                         </div>
@@ -48,12 +48,12 @@ function FriendPage() {
                     </Routes>
 
                     <NavLink to="/friendpage/allFriends">
-                        <div className="friendpageLeftbarBottomOptions">
-                            <div className="friendpageLeftbarBottomOptionsLeft">
+                        <div className="friendpageLeftbarBottomOption">
+                            <div className="friendpageLeftbarBottomOptionLeft">
                                 <PersonAddAlt1Icon />
                                 <p>All friends</p>
                             </div>
-                            <div className="friendpageLeftbarBottomOptionsRight">
+                            <div className="friendpageLeftbarBottomOptionRight">
                                 <ArrowForwardIosIcon />
                             </div>
                         </div>
